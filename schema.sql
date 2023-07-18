bedeaed4b57164c1e70eaddce7fefc2667ff3715
Sandeep Ghosh <mailsg0912@gmail.com>
2023-07-18 20:18:19 +0530

    Query multiple tables

--- schema.sql
+++ schema.sql
@@ -13,3 +13,10 @@ CREATE TABLE animals(
 /* Add a column species of type string to the table. */
 ALTER TABLE animals ADD species VARCHAR(50);
 
+/* Adding new tables and modifying existing tables */
+CREATE TABLE owners(id INT NOT NULL GENERATED ALWAYS AS IDENTITY, full_name VARCHAR(50), age INT);
+CREATE TABLE species(id INT NOT NULL GENERATED ALWAYS AS IDENTITY, name VARCHAR(50), PRIMARY KEY(id));
+ALTER TABLE owners ADD CONSTRAINT owners_pkey PRIMARY KEY(id);
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id INT, ADD CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id) ON DELETE CASCADE;
+ALTER TABLE animals ADD owner_id INT, ADD CONSTRAINT fk_owners FOREIGN KEY(owner_id) REFERENCES owners(id) ON DELETE CASCADE;
